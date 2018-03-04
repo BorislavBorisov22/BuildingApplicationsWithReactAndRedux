d6dbd93c0933dfce45b5bd5f8500875cda976306
BorislavBorisov22 <borislavborisov22@abv.bg>
2018-03-05 00:18:05 +0200

    Added course page

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,17 @@
+import React, {PropTypes} from 'react';
+
+class CoursesPage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Courses</h1>
+            </div>
+        );
+    }
+}
+
+export default CoursesPage;
