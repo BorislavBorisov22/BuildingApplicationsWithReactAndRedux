d2cd3936a836ee9445946030e64f5c5c0eae9e22
BorislavBorisov22 <borislavborisov22@abv.bg>
2018-03-05 20:03:13 +0200

    added selectInput component and added textinput and selectinput to CourseFrom component

--- src/components/course/CourseForm.js
+++ src/components/course/CourseForm.js
@@ -1,8 +1,48 @@
 import React, {PropTypes} from 'react';
-import TextInput from '../common/textInput';
+import TextInput from './../common/TextInput';
+import SelectInput from './../common/SelectInput';
 
-const CourseForm =  ({course, allAuthors, onSave, onChange, loading, error}) => {
+const CourseForm =  ({course, allAuthors, onSave, onChange, loading, errors}) => {
+    return (
+        <form>
+            <h1>Manage Course</h1>
+            <TextInput 
+                name="title"
+                label="title"
+                value={course.title}
+                onChange={onChange}
+                error={errors.title}
+            />
 
+            <SelectInput 
+                name="authorId"
+                label="authorId"
+                value={course.authorId}
+                defaultOption="Select Author"
+                options={allAuthors}
+                onChange={onChange}
+                error={errors.authorId}
+            />
+
+             <TextInput 
+                name="category"
+                label="category"
+                value={course.category}
+                onChange={onChange}
+                error={errors.category}
+            />
+
+             <TextInput 
+                name="length"
+                label="lenght"
+                value={course.length}
+                onChange={onChange}
+                error={errors.title}
+            />
+
+            <input onClick={onSave} type="button" className="btn btn-default"  value="Save"/>
+        </form>
+    );
 };
 
 CourseForm.propTypes = {
@@ -11,7 +51,7 @@ CourseForm.propTypes = {
     onSave: PropTypes.func.isRequired,
     onChange: PropTypes.func.isRequired,
     loading: PropTypes.bool,
-    error: PropTypes.object
+    errors: PropTypes.object
 };
 
 export default CourseForm;
