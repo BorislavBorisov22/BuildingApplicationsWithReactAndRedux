a871ef071f66d1afd28cfc1cee3e5f40aa0d4a29
BorislavBorisov22 <borislavborisov22@abv.bg>
2018-03-05 16:15:35 +0200

    Extracted presentation components for listing available courses

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,6 +2,7 @@ import React, {PropTypes} from 'react';
 import { connect } from "react-redux";
 import * as courseActions from "../../actions/courseActions";
 import { bindActionCreators } from 'redux';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
     constructor(props, context) {
@@ -18,7 +19,7 @@ class CoursesPage extends React.Component {
         return (
             <div>
                 <h1>Courses</h1>
-                {this.props.courses.map(this.courseRow)}
+                <CourseList courses={this.props.courses} />
             </div>
         );
     }
