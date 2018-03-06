556a0405fecde3af98986a292b5b2afa47302692
BorislavBorisov22 <borislavborisov22@abv.bg>
2018-03-06 21:30:50 +0200

    Added test duplicates written with enzyme

--- src/components/course/CourseForm.Enzyme.test.js
+++ src/components/course/CourseForm.Enzyme.test.js
@@ -1,1 +1,35 @@
-import { React } from 'react';
+import React from 'react';
+import expect from 'expect';
+import { mount, shallow } from 'enzyme';
+import TestUtils from 'react-addons-test-utils';
+import CourseForm from './CourseForm';
+
+function setup(saving = false) {
+    const props = {
+        course: {},
+        saving,
+        loading: false,
+        errors: {},
+        onSave: () => {},
+        onChange: () => {},
+        allAuthors: []
+    };
+
+    return shallow(<CourseForm {...props}/>);
+}
+
+it('renders form and h1', () => {
+    const wrapper = setup();
+    expect(wrapper.find('form').length).toBe(1);
+    expect(wrapper.find('h1').text()).toEqual('Manage Course');
+});
+
+it('save button is labeled Save when not saving', () => {
+    const wrapper = setup();
+    expect(wrapper.find('input').props().value).toEqual('Save');
+});
+
+it('save button is labeled Saving.. when saving', () => {
+    const wrapper = setup(true);
+    expect(wrapper.find('input').props().value).toEqual('Saving...');
+});
