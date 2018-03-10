8b5e0b7286bdf4a35fb343eff647dd6002bec7f0
BorislavBorisov22 <borislavborisov22@abv.bg>
2018-03-10 21:43:47 +0200

    Added delete authros action and action type

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -1,9 +1,13 @@
 import authorApi from './../api/mockAuthorApi';
-import { LOAD_AUTHORS_SUCCESS } from './actionTypes';
+import { LOAD_AUTHORS_SUCCESS, DELETE_AUTHOR_SUCCESS } from './actionTypes';
 import { beginAjaxCall } from './ajaxStatusActions';
 
 export function loadAuthorsSuccess(authors) {
     return { type: LOAD_AUTHORS_SUCCESS, authors };
+}
+
+export function deleteAuthorSuccess() {
+    return { type: DELETE_AUTHOR_SUCCESS };
 }
 
 export function loadAuthors() {
@@ -16,3 +20,14 @@ export function loadAuthors() {
         });
     };
 } 
+
+export function deleteAuthor(authorId) {
+    return dispatch => {
+        dispatch(beginAjaxCall());
+        return authorApi.deleteAuthor(authorId).then(() => {
+            dispatch(deleteAuthorSuccess());
+        }).catch(err => {
+            throw err;
+        });
+    };
+}
